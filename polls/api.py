2728dea67806d5eb93541badddceed26617fdeef
snowcxt <snowcxt@gmail.com>
2017-09-26 22:53:04 -0700

    create api

---
+++ polls/api.py
@@ -0,0 +1,7 @@
+from rest_framework.generics import ListAPIView
+from .serializers import QuestionSerializer
+from .models import Question
+
+class QuestionApi(ListAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
+from .api import QuestionApi
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^question$', QuestionApi.as_view())
 ]
