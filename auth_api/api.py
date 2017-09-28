6547f13ddec057bc7f26a2c5fb6c50b2942b943f
snowcxt <snowcxt@gmail.com>
2017-09-28 10:21:47 -0700

    authenticate api

--- auth_api/api.py
+++ auth_api/api.py
@@ -1,23 +1,29 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework import status, views
-from django.contrib.auth import authenticate, login
-from rest_framework.response import Response
+from django.http import JsonResponse, HttpResponse
+from rest_framework import status
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import csrf_exempt
 
 
-def load_auth(request):
-    return JsonResponse({"id": 1, "username": "testuser"})
+def load_auth_view(request):
+    if request.user.is_authenticated:
+        return JsonResponse({"id": request.user.id, "username":  request.user.username})
+    else:
+        return HttpResponse('')
 
 
-class LoginView(views.APIView):
-    def post(self, request):
-        user = authenticate(
-            username=request.data.get("username"),
-            password=request.data.get("password"))
-        if user is None or not user.is_active:
-            return Response({
-                'status': 'Unauthorized',
-                'message': ''
-            }, status=status.HTTP_401_UNAUTHORIZED)
-        login(request, user)
-        return Response({'id': 0})
+@csrf_exempt
+def login_view(request):
+    user = authenticate(
+        username=request.POST.get("username"),
+        password=request.POST.get("password"))
+    if user is None or not user.is_active:
+        return JsonResponse({
+            'status': 'Unauthorized',
+        }, status=status.HTTP_401_UNAUTHORIZED)
+    login(request, user)
+    return JsonResponse({'id': user.id, 'username': user.username})
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponse('')
--- auth_api/urls.py
+++ auth_api/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import api
 
 urlpatterns = [
-    url(r'^load-auth$', api.load_auth),
+    url(r'^load-auth$', api.load_auth_view),
+    url(r'^login$', api.login_view),
+    url(r'^logout$', api.logout_view),
 ]
