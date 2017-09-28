fe3f45a85c9216a89bbdaff047c833beae0441a6
snowcxt <snowcxt@gmail.com>
2017-09-27 17:01:17 -0700

    create login

--- auth_api/api.py
+++ auth_api/api.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework import status, views
+from django.contrib.auth import authenticate, login
+from rest_framework.response import Response
+
 
 def load_auth(request):
-    return JsonResponse({ "id": 1, "username": "testuser" })
+    return JsonResponse({"id": 1, "username": "testuser"})
+
+
+class LoginView(views.APIView):
+    def post(self, request):
+        user = authenticate(
+            username=request.data.get("username"),
+            password=request.data.get("password"))
+        if user is None or not user.is_active:
+            return Response({
+                'status': 'Unauthorized',
+                'message': ''
+            }, status=status.HTTP_401_UNAUTHORIZED)
+        login(request, user)
+        return Response({'id': 0})
